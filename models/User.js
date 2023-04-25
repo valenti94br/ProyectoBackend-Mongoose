956fbdbf299746607d29739d2ee4c477d5b68fb0
valenti94br <valenti94br@gmail.com>
2023-04-25 15:43:55 +0200

    Creando modelo user

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.SchemaTypes.ObjectId;
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Por favor rellena tu nombre"],
+    },
+    email: {
+      type: String,
+      match: [/.+\@.+\..+/, "Este correo no es válido"],
+      unique: true,
+      required: [true, "Por favor rellena tu correo"],
+    },
+    password: {
+      type: String,
+      required: [true, "Por favor rellena tu contraseña"],
+    },
+    age: {
+      type: Number,
+      required: [true, "Por favor rellena tu edad"],
+    },
+    role: { type: String, default: "user" },
+    tokens: [],
+    orderIds: [{ type: ObjectId, ref: "Order" }],
+    wishList: [{ type: ObjectId, ref: 'Post' }],
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods.toJSON = function () {
+  const user = this._doc;
+  delete user.tokens;
+  delete user.password;
+  return user;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
