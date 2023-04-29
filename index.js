d41fe2487ec77a9854774b538820e91b9e0ab924
Goner10 <zalito_96@hotmail.com>
2023-04-29 14:27:52 +0200

    vercel config added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
 const { dbConnection } = require("./config/config")
 const { handleTypeError } = require("./middleware/errors");
 const { authentication } = require("./middleware/authentication")
